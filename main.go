caffcf1b3e49f953ce535f9ee5bf97130f257661
Pritish Mishra <pritishmishra579@gmail.com>
2024-05-09 14:58:25 +0530

    chore: Update hostname variable to use r.Host instead of r.URL.Host

--- main.go
+++ main.go
@@ -27,13 +27,12 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("Request for %s", r.URL.Host)
 	BASE_PATH, _ := os.LookupEnv("BASE_PATH")
-
 	if BASE_PATH == "" {
 		log.Fatal("BASE_PATH is not set")
 	}
-	hostname := r.URL.Host
+	
+	hostname := r.Host
 	subdomain := strings.Split(hostname, ".")[0]
 	log.Printf("Request for subdomain: %s", subdomain)
 
